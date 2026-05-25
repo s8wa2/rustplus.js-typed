35b02a0bdce60c6f6b122f610c2a79a529860aa8
s8wa2 <86946027+s8wa2@users.noreply.github.com>
2026-05-24 20:10:20 -0700

    get rid of extra string => string conversion

---
+++ .changeset/thirty-books-sleep.md
@@ -0,0 +1,5 @@
+---
+"@grolm/rustplus.js-typed": patch
+---
+
+Getting rid of an unecessary string to string conversion
--- src/rustplus.ts
+++ src/rustplus.ts
@@ -141,7 +141,7 @@ export class RustPlus extends (EventEmitter as new () => TypedEmitter<RustPlusEv
     this.port = port;
     this.playerId = playerId;
     this.playerToken = playerToken;
-    this._playerId = String(playerId);
+    this._playerId = playerId;
     this._playerToken = Number(playerToken);
     this.useFacepunchProxy = useFacepunchProxy;
 
